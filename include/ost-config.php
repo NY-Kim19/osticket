54c5ed3913e34f80ae9aec0f8dcc2382d032a17c
NY-Kim19 <74401233+NY-Kim19@users.noreply.github.com>
2021-02-04 16:22:03 +0900

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,10 +39,10 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','yourservername.mysql.database.azure.com');
+define('DBHOST','user02dbsvr01.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@yourservername');
-define('DBPASS','demo@pass123');
+define('DBUSER','user02@user02dbsvr01');
+define('DBPASS','user@pass02');
 
 # Table prefix
 define('TABLE_PREFIX','ost_');
